40c124c1f91ce7a111aa78e3c10cc261792aaeff
Pedro Henrique <pedro_q777@live.com>
2024-10-26 15:56:48 -0300

    Criando nova classe FirewallService

--- src/main/java/edu/catolica/api/mikrotik/connection/service/FirewallService.java
+++ src/main/java/edu/catolica/api/mikrotik/connection/service/FirewallService.java
@@ -1,11 +1,30 @@
 package edu.catolica.api.mikrotik.connection.service;
 
-import me.legrange.mikrotik.ApiConnection;
-import me.legrange.mikrotik.MikrotikApiException;
-import org.springframework.beans.factory.annotation.Value;
+import edu.catolica.api.mikrotik.connection.infra.MikroTikApiClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-import javax.net.SocketFactory;
+import java.util.List;
+import java.util.Map;
 
+@RequiredArgsConstructor
+@Service
 public class FirewallService {
+    private final MikroTikApiClient mikroTikApiClient;
 
+    public void adicionarBloqueio(String name) {
+
+    }
+
+    public void removerBloqueio(String name) {
+
+    }
+
+    public void atualizarBloqueio(String name) {
+
+    }
+
+    private List<Map<String, String>> encontrarBloqueio(String name) {
+        return null;
+    }
 }
