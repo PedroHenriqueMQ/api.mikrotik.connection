0042e1efd10ab3e934de45a5853424207b7a6ed2
Pedro Henrique <pedro_q777@live.com>
2024-11-16 12:18:59 -0300

    Adição de retorno aos métodos do UsuarioController

--- src/main/java/edu/catolica/api/mikrotik/connection/controller/UsuarioController.java
+++ src/main/java/edu/catolica/api/mikrotik/connection/controller/UsuarioController.java
@@ -1,40 +1,35 @@
 package edu.catolica.api.mikrotik.connection.controller;
 
 import edu.catolica.api.mikrotik.connection.dto.UsuarioCadastro;
-import edu.catolica.api.mikrotik.connection.dto.UsuarioLogin;
 import edu.catolica.api.mikrotik.connection.service.UsuarioService;
 import jakarta.validation.Valid;
 import me.legrange.mikrotik.MikrotikApiException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
 
-    @PostMapping("/login")
-    void login(@Valid @RequestBody UsuarioLogin usuarioLogin) {
-
-    }
-
-    @PostMapping("/logout")
-    void logout() {
-
-    }
-
     @PostMapping("/create-account")
-    void createAccount(@Valid @RequestBody UsuarioCadastro usuarioCadastro) throws MikrotikApiException {
+    ResponseEntity<Void> createAccount(@Valid @RequestBody UsuarioCadastro usuarioCadastro) throws MikrotikApiException {
         usuarioService.criarConta(usuarioCadastro);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/delete-account/{usuario}")
-    void deleteAccount(@Valid @PathVariable String usuario) throws MikrotikApiException {
+    ResponseEntity<Void> deleteAccount(@PathVariable String usuario) throws MikrotikApiException {
         usuarioService.apagarConta(usuario);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PutMapping("/update-account")
-    void updateAccount(@Valid @RequestBody UsuarioCadastro usuario) throws MikrotikApiException {
+    ResponseEntity<Void> updateAccount(@Valid @RequestBody UsuarioCadastro usuario) throws MikrotikApiException {
         usuarioService.atualizarConta(usuario);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
