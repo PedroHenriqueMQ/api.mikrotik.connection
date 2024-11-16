b59b93e1f6ecf891a84e20ded6a1cc6ecf4e6814
Pedro Henrique <pedro_q777@live.com>
2024-11-16 12:15:26 -0300

    Novas funcionalidades no FirewallService
    - Método de inicialização de instância da classe
    - Método para adicionar novo bloqueio
    - Método para encontrar lista de bloqueios
    - Variáveis constantes com valores globais

--- src/main/java/edu/catolica/api/mikrotik/connection/service/FirewallService.java
+++ src/main/java/edu/catolica/api/mikrotik/connection/service/FirewallService.java
@@ -1,19 +1,45 @@
 package edu.catolica.api.mikrotik.connection.service;
 
 import edu.catolica.api.mikrotik.connection.infra.MikroTikApiClient;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import me.legrange.mikrotik.MikrotikApiException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class FirewallService {
     private final MikroTikApiClient mikroTikApiClient;
+    private final String blockListName = "block_sites";
+    private final String[] initialRegexpPattern = new String[]{"^.+(", ").*$"};
 
-    public void adicionarBloqueio(String name) {
+    @PostConstruct
+    private void criarListaDeBloqueios() throws MikrotikApiException {
+        if (encontrarListaDeBloqueios() != null)
+            return;
 
+        var comandoCriarNovo = String.format(
+                "/ip/firewall/layer7-protocol/add name=%s regexp=\"%s\"",
+                this.blockListName,
+                String.join("", initialRegexpPattern)
+        );
+        mikroTikApiClient.getConnection().execute(comandoCriarNovo);
+    }
+
+    public void adicionarBloqueio(String name) throws MikrotikApiException {
+        var listaDeBloqueios = encontrarListaDeBloqueios();
+        var regexp = listaDeBloqueios.get("regexp");
+
+        if (!regexp.contains(initialRegexpPattern[0]) || !regexp.contains(initialRegexpPattern[1]))
+            return;
+
+        var nameBlock = regexp.contentEquals(String.join("", initialRegexpPattern)) ? name : name + "|";
+        regexp = String.valueOf(new StringBuilder(regexp).insert(initialRegexpPattern[0].length(), nameBlock));
+        var comandoAdicionar = String.format("/ip/firewall/layer7-protocol/set .id=%s regexp=\"%s\"", listaDeBloqueios.get(".id"), regexp);
+
+        mikroTikApiClient.getConnection().execute(comandoAdicionar);
     }
 
     public void removerBloqueio(String name) {
@@ -24,7 +50,13 @@ public class FirewallService {
 
     }
 
-    private List<Map<String, String>> encontrarBloqueio(String name) {
-        return null;
+    private Map<String, String> encontrarListaDeBloqueios() throws MikrotikApiException {
+        var comandoPesquisar = String.format("/ip/firewall/layer7-protocol/print where name=%s", this.blockListName);
+        var resultadoPesquisa = mikroTikApiClient.getConnection().execute(comandoPesquisar);
+
+        if (resultadoPesquisa.isEmpty())
+            return null;
+
+        return resultadoPesquisa.getFirst();
     }
 }
